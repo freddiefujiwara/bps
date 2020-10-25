7c69ab7ca499d6debe2d68d28962b415ee20ce2e
Fumikazu Fujiwara <github@ze.gs>
2020-10-25 21:03:19 +0900

    add rakuma

--- android/app/src/main/kotlin/com/example/bps/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bps/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.freddiefujiwara.bps
+package com.example.bps
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:web_scraper/web_scraper.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final rakumaProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
+  final webScraper = WebScraper('https://fril.jp');
+  List<Map<String, dynamic>> items = [];
+  if (await webScraper.loadWebPage('/s?category_id=733&query=flutter')) {
+    Map<String, dynamic> item;
+    List<Map<String, dynamic>> titles = webScraper.getElement(
+        'div.item-box__image-wrapper > a.link_search_image > img',
+        ['alt', 'data-original']);
+    List<Map<String, dynamic>> links = webScraper.getElement(
+        'div.item-box__image-wrapper > a.link_search_image', ['href']);
+    List<Map<String, dynamic>> prices =
+        webScraper.getElement('span[itemProp="price"]', ['data-content']);
+    titles.asMap().forEach((i, title) {
+      item = {
+        "title": title["attributes"]["alt"],
+        "img": title["attributes"]["data-original"],
+        "link": links[i]["attributes"]["href"],
+        "price": prices[i]["attributes"]["data-content"]
+      };
+      items.add(item);
+    });
+  }
+  return items;
+});
 
 void main() {
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Product Catalog"),
+          ),
+          body: MyHome()));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHome extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    final AsyncValue<List<Map<String, dynamic>>> list =
+        useProvider(rakumaProvider);
+
+    return list.when(
+        loading: () => const CircularProgressIndicator(),
+        error: (err, stack) => Text('Error: $err'),
+        data: (items) {
+          return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                    child: InkWell(
+                  onTap: () async {
+                    String url = items[index]['link'];
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                  child:
+                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    Text("${items[index]['title']}"),
+                    Text("${items[index]['price']}"),
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.network(items[index]['img']))
+                  ]),
+                ));
+              });
+        });
   }
 }
